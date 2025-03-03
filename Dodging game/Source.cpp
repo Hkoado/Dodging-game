00f94c9947943c7830f536a4c6d122ffd5004974
Hkoado <24022805@vnu.edu.vn>
2025-03-03 15:07:56 +0700

    Add project files.

---
+++ Dodging game/Source.cpp
@@ -0,0 +1,94 @@
+﻿#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+bool init(SDL_Window*& window, SDL_Renderer*& renderer) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL không thể khởi tạo! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    window = SDL_CreateWindow("SDL2 Window",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cout << "Không thể tạo cửa sổ! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cout << "Không thể tạo renderer! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        std::cout << "SDL_image không thể khởi tạo! IMG Error: " << IMG_GetError() << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+SDL_Texture* loadTexture(const std::string& path, SDL_Renderer* renderer) {
+    SDL_Texture* texture = nullptr;
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if (!loadedSurface) {
+        std::cout << "Không thể tải ảnh! IMG Error: " << IMG_GetError() << std::endl;
+        return nullptr;
+    }
+
+    texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    SDL_FreeSurface(loadedSurface);
+
+    if (!texture) {
+        std::cout << "Không thể tạo texture từ ảnh! SDL Error: " << SDL_GetError() << std::endl;
+    }
+
+    return texture;
+}
+
+void close(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* texture) {
+    if (texture) SDL_DestroyTexture(texture);
+    if (renderer) SDL_DestroyRenderer(renderer);
+    if (window) SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    if (!init(window, renderer)) {
+        return -1;
+    }
+
+    SDL_Texture* imageTexture = loadTexture("background.png", renderer);
+    if (!imageTexture) {
+        close(window, renderer, nullptr);
+        return -1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, imageTexture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
+    }
+
+    close(window, renderer, imageTexture);
+    return 0;
+}
